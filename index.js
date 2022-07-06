1c4b4fed86e54a2d66d4ccd5a41cae370797ca71
Arman Ali <101466326+arman8808@users.noreply.github.com>
2022-07-06 14:29:00 +0530

    web

--- index.js
+++ index.js
@@ -43,6 +43,96 @@ function Moreless(){
         Read.innerHTML="Read More"
     }
 }
+function Moreless1(){
+    let dots=document.getElementById("dots1");
+    console.log(dots)
+    let invesibleText=document.getElementById("invesible1");
+    console.log(invesibleText)
+    let Read=document.getElementById("read-more1");
+    console.log(Read)
+    if(dots.style.display!='none'){
+        dots.style.display='none';
+        invesibleText.style.display='inline';
+        Read.innerHTML="Read Less"
+    }
+    else{
+        dots.style.display='inline';
+        invesibleText.style.display='none';
+        Read.innerHTML="Read More"
+    }
+}
+function Moreless2(){
+    let dots=document.getElementById("dots2");
+    console.log(dots)
+    let invesibleText=document.getElementById("invesible2");
+    console.log(invesibleText)
+    let Read=document.getElementById("read-more2");
+    console.log(Read)
+    if(dots.style.display!='none'){
+        dots.style.display='none';
+        invesibleText.style.display='inline';
+        Read.innerHTML="Read Less"
+    }
+    else{
+        dots.style.display='inline';
+        invesibleText.style.display='none';
+        Read.innerHTML="Read More"
+    }
+}
+function Moreless3(){
+    let dots=document.getElementById("dots3");
+    console.log(dots)
+    let invesibleText=document.getElementById("invesible3");
+    console.log(invesibleText)
+    let Read=document.getElementById("read-more3");
+    console.log(Read)
+    if(dots.style.display!='none'){
+        dots.style.display='none';
+        invesibleText.style.display='inline';
+        Read.innerHTML="Read Less"
+    }
+    else{
+        dots.style.display='inline';
+        invesibleText.style.display='none';
+        Read.innerHTML="Read More"
+    }
+}
+function Moreless4(){
+    let dots=document.getElementById("dots4");
+    console.log(dots)
+    let invesibleText=document.getElementById("invesible4");
+    console.log(invesibleText)
+    let Read=document.getElementById("read-more4");
+    console.log(Read)
+    if(dots.style.display!='none'){
+        dots.style.display='none';
+        invesibleText.style.display='inline';
+        Read.innerHTML="Read Less"
+    }
+    else{
+        dots.style.display='inline';
+        invesibleText.style.display='none';
+        Read.innerHTML="Read More"
+    }
+}
+function Moreless5(){
+    let dots=document.getElementById("dots5");
+    console.log(dots)
+    let invesibleText=document.getElementById("invesible5");
+    console.log(invesibleText)
+    let Read=document.getElementById("read-more5");
+    console.log(Read)
+    if(dots.style.display!='none'){
+        dots.style.display='none';
+        invesibleText.style.display='inline';
+        Read.innerHTML="Read Less"
+    }
+    else{
+        dots.style.display='inline';
+        invesibleText.style.display='none';
+        Read.innerHTML="Read More"
+    }
+}
 // responsive
 function toggle() {
     let x =document.getElementById("hambarger");
